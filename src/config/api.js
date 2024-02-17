0a3d2569b86d340b9c671305e62d6cc88b011117
Rendy Christian <rendychristiann@gmail.com>
2024-02-17 19:22:25 +0700

    change url API

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://skripsi-airsense-backend.onrender.com/',
 });
 
 export const getAll = async () => {
