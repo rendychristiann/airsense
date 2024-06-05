dfbd1df63dbc8f7e281faa49d53d18f290c567ad
Rendy Christian <rendychristiann@gmail.com>
2024-06-05 17:18:50 +0700

    fix chart width (prod)

--- src/components/dashboardComponents/ChartSection.jsx
+++ src/components/dashboardComponents/ChartSection.jsx
@@ -4,10 +4,16 @@ import RSSIChart from "./RSSIChart";
 
 const ChartSection = ({ indexData }) => {
   return (
-    <div className="flex flex-wrap justify-center items-center">
-      <PM25Chart indexData={indexData} />
-      <SNRChart indexData={indexData} />
-      <RSSIChart indexData={indexData} />
+    <div className="flex flex-col justify-center items-center min-h-screen p-4">
+      <div className="w-full max-w-3xl mb-4 px-4 sm:px-6 lg:px-8">
+        <PM25Chart indexData={indexData} />
+      </div>
+      <div className="w-full max-w-3xl mb-4 px-4 sm:px-6 lg:px-8">
+        <SNRChart indexData={indexData} />
+      </div>
+      <div className="w-full max-w-3xl mb-4 px-4 sm:px-6 lg:px-8">
+        <RSSIChart indexData={indexData} />
+      </div>
     </div>
   );
 };
